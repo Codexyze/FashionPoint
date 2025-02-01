7d1040bf3244bfd1d68b5f08c1ac8550d6afc644
Akshay Sarapure <akshaysarapure@gmail.com>
2025-02-01 16:15:34 +0530

    Version 1.4.1 ###Search Feature drop

--- app/src/main/java/com/example/femalepoint/MainActivity.kt
+++ app/src/main/java/com/example/femalepoint/MainActivity.kt
@@ -49,48 +49,13 @@ class MainActivity : ComponentActivity(),PaymentResultListener {
             }
 
         }
-    fun startPayment() {
-        //payment gateway
-        /*
-        *  You need to pass the current activity to let Razorpay create CheckoutActivity
-        * */
-        val activity: Activity = this
-        val co = Checkout()
-
-        try {
-            val options = JSONObject()
-            options.put("name","Razorpay Corp")
-            options.put("description","Demoing Charges")
-            //You can omit the image option to fetch the image from the dashboard
-            options.put("image","http://example.com/image/rzp.jpg")
-            options.put("theme.color", "#3399cc");
-            options.put("currency","INR");
-            options.put("order_id", "order_DBJOWzybf0sJbb");
-            options.put("amount","50000")//pass amount in currency subunits
-
-            val retryObj =  JSONObject()
-            retryObj.put("enabled", true);
-            retryObj.put("max_count", 4);
-            options.put("retry", retryObj);
-
-            val prefill = JSONObject()
-            prefill.put("email","gaurav.kumar@example.com")
-            prefill.put("contact","9876543210")
-
-            options.put("prefill",prefill)
-            co.open(activity,options)
-        }catch (e: Exception){
-            Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
-            e.printStackTrace()
-        }
-    }
 
     override fun onPaymentSuccess(p0: String?) {
         TODO("Not yet implemented")
     }
 
     override fun onPaymentError(p0: Int, p1: String?) {
-        TODO("Not yet implemented")
+        Toast.makeText(this, "Payment Failed", Toast.LENGTH_SHORT).show()
 
     }
 
